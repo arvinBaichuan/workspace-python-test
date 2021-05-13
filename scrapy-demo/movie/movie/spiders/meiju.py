516cbb84417c14afd4d6a5919b1563d76a1e7a49
lbc <19610571+arvinBaichuan@users.noreply.github.com>
2021-05-13 12:15:12 +0800

    #1

--- scrapy-demo/movie/movie/spiders/meiju.py
+++ scrapy-demo/movie/movie/spiders/meiju.py
@@ -5,16 +5,17 @@ import scrapy
 from items import MovieItem
 
 class MeijuSpider(scrapy.Spider):
+    print('测试1-》')
     name = 'meiju'
     allowed_domains = ['www.meijutt.tv']
     start_urls = ['http://www.meijutt.tv/']
 
     def parse(self, response):
         movies = response.xpath('//ul[@class="top-list  fn-clear"]/li')
-        print('测试1-》')
+        print('测试2-》')
         print(movies)
         for each_movie in movies:
-            print('测试-》'+each_movie)
+            print('测试3-》'+each_movie)
             item = MovieItem()
             item['name'] = each_movie.xpath('./h5/a/@title').extract()[0]
             yield item
